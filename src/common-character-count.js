95e98365bb1f47cb9a0f040874ac770c7d1a6156
p0lluxstar <web-registra@yandex.ru>
2023-04-15 18:54:23 +0300

    150423-18:54

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+
+let arr1 = [];
+let arr2 = [];
+let arr3 = [];
+
+for (let i = 0; i < s1.length; i++){
+  arr1[i] = s1[i]
+}
+
+for (let i = 0; i < s2.length; i++){
+  arr2[i] = s2[i]
+}
+
+count = 0;
+
+arr1.forEach((el, index) =>{
+  for(let i = 0; i < arr2.length; i++){
+    if(el === arr2[i]){
+      arr3[count] = el;
+      arr2.splice(i, 1)
+      i += arr1.length
+      count++;
+    }
+  }
+})
+
+if(arr3.length > arr1.length){
+  return arr1.length
+}
+
+return arr3.length
+
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  
+  if (!Array.isArray(arr)){
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+
+  if(arr[0] === '--discard-prev' || arr[0] === '--double-prev'){
+    arr.splice(0, 1)
+
+  }
+  
+  if(arr[arr.length-1] === '--double-next' || arr[arr.length-1] === '--discard-next'){
+    arr.splice(arr.length-1)
+  }
+
+  console.log(arr)
+  
+  let newArr = [];
+
+ for (let i = 0, j = 0; i < arr.length; i++){
+  if (arr[i] === '--discard-next'){
+    i++
+  }else if(arr[i] === '--discard-prev'){
+    newArr.splice(i-1)
+    j--
+  }else if (arr[i] === '--double-next'){
+    newArr[j] = arr[i + 1] 
+    j++ 
+  }else if(arr[i] === '--double-prev'){
+    newArr[j] = arr[i - 1] 
+    j++
+  }else {
+    newArr[j] = arr[i];
+    j++
+  }
+ }
+
+ console.log(newArr)
+
+ return newArr;
+
 }
+
+transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5])
 
 module.exports = {
   transform
 };
+
+/* let a =['--discard-prev', 1, 2, 3]
+console.log(a.splice(0, 1))
+console.log(a) */
